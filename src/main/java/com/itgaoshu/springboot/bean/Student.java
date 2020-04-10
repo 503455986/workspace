4f019f2fe4776852325d05c31ac735161789d453
chenjiangq <1808299016@qq.com>
2020-04-10 14:55:02 +0800

    hhh

--- src/main/java/com/itgaoshu/springboot/bean/Student.java
+++ src/main/java/com/itgaoshu/springboot/bean/Student.java
@@ -16,6 +16,7 @@ public class Student implements Serializable {
     private Integer stuid;
     private String stuname;
     private double scose;
+    private int isd;
 
     public Integer getStuid() {
         return stuid;
@@ -41,12 +42,21 @@ public class Student implements Serializable {
         this.scose = scose;
     }
 
+    public int getIsd() {
+        return isd;
+    }
+
+    public void setIsd(int isd) {
+        this.isd = isd;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
                 "stuid=" + stuid +
                 ", stuname='" + stuname + '\'' +
                 ", scose=" + scose +
+                ", isd=" + isd +
                 '}';
     }
 }
